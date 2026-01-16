9f972faed3ed96fe978a96b0ecaf3b76efed417b
DjineSP <dsintopafing@gmail.com>
2026-01-16 01:09:07 +0100

    Configuration du dokerfile et utilisation de .env

--- src/main/java/com/sharepay/discovery/SharepayDiscoveryApplication.java
+++ src/main/java/com/sharepay/discovery/SharepayDiscoveryApplication.java
@@ -1,5 +1,6 @@
 package com.sharepay.discovery;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,6 +9,16 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SharepayDiscoveryApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			String key = entry.getKey();
+			String value = entry.getValue();
+
+			if (System.getenv(key) == null && System.getProperty(key) == null) {
+				System.setProperty(key, value);
+			}
+		});
+
 		SpringApplication.run(SharepayDiscoveryApplication.class, args);
 	}
 }
